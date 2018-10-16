ed148678df0966405fd790e7738be44e712bd1ce
David Ortiz <z.david.ortiz@gmail.com>
2018-10-16 10:53:47 +0200

    policies/upstream: adapt to new Upstream interface
    The interface introduced in APIcast 3.3.0.

--- apicast/policies/cloud_hosted.upstream/0.1/upstream.lua
+++ apicast/policies/cloud_hosted.upstream/0.1/upstream.lua
@@ -1,6 +1,4 @@
-local resty_resolver = require('resty.resolver')
-local resty_url = require('resty.url')
-local format = string.format
+local Upstream = require('apicast.upstream')
 
 local _M = require('apicast.policy').new('Upstream', '0.1')
 
@@ -11,27 +9,13 @@ function _M.new(configuration)
   local policy = new(configuration)
   local config = configuration or empty
 
-  local url = resty_url.parse(config.url) or empty
-  local host = config.host or url.host
-
-  policy.host = host
-  policy.url = url
+  policy.upstream = Upstream.new(config.url)
 
   return policy
 end
 
-function _M:content()
-  local url = self.url
-  local host = self.host
-
-  ngx.ctx.upstream = resty_resolver:instance():get_servers(url.host, { port = url.port })
-  ngx.var.proxy_pass = format('%s://upstream%s', url.scheme, url.path or '')
-  ngx.req.set_header('Host', host or ngx.var.host)
-
-  if not ngx.headers_sent then
-    ngx.exec("@upstream")
-  end
+function _M:content(context)
+  self.upstream:call(context)
 end
-
 
 return _M
