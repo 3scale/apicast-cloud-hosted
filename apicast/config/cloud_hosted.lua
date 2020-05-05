3a04c30e043b0af26556fcf35569226b28902942
Roi Vazquez <roivazquez@gmail.com>
2020-05-05 11:21:57 +0200

    Remove rate_limit policy and unused module resources

--- apicast/config/cloud_hosted.lua
+++ apicast/config/cloud_hosted.lua
@@ -2,11 +2,6 @@ local PolicyChain = require('apicast.policy_chain')
 local policy_chain = context.policy_chain
 
 if not arg then -- {arg} is defined only when executing the CLI
-  policy_chain:insert(PolicyChain.load_policy('cloud_hosted.rate_limit', '0.1', {
-    limit = os.getenv('APICAST_RATE_LIMIT') or os.getenv('RATE_LIMIT') or 5,
-    burst = os.getenv('APICAST_RATE_LIMIT_BURST') or os.getenv('RATE_LIMIT_BURST') or 50,
-    status = os.getenv('APICAST_RATE_LIMIT_STATUS') or os.getenv('RATE_LIMIT_STATUS'),
-  }), 1)
   policy_chain:insert(PolicyChain.load_policy('cloud_hosted.balancer_blacklist', '0.1'), 1)
 end
 
